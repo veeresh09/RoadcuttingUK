b16ad1fc1049aac17450d3f5a8c874b98ec3c492
veeresh09 <30980472+veeresh09@users.noreply.github.com>
2020-03-31 14:03:07 +0530

    Update Dashboard.js

--- RoadCutting/client/src/components/layouts/Dashboard.js
+++ RoadCutting/client/src/components/layouts/Dashboard.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux"; //import redux
 import Menu from "./Menu"; //To use top menu component;
 import Card from "react-bootstrap/Card"; //import card to diplay existing forms as card
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 import { CardGroup } from "react-bootstrap";
-import digit from "../../assets/digit.svg";
-
-export default class Dashboard extends Component {
+class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
+  async componentDidMount() {
+    //To get the district list from database using api calls as soon as component displays
+    try {
+      const auth_token = this.props.auth_token;
+      const body = JSON.stringify({ auth_token });
+      const config = {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+      const res = await axios.post("/form/cities", body, config);
+      console.log(res);
+      res.data.wardnumbers[0] = "Select ward Number";
+      await this.props.load_data(res.data);
+    } catch (error) {
+      alert(error);
+    }
+  }
   render() {
     return (
       <div>
@@ -37,12 +54,43 @@ export default class Dashboard extends Component {
             </Link>
           </div>
         </div>
-
-        <div className="d-flex justify-content-center align-items-center mt-5 mb-2 footer">
-          <h6 className="mb-0">Powered by </h6>{" "}
-          <img src={digit} className="ml-2" alt="Digit" height="20" />
-        </div>
       </div>
     );
   }
 }
+
+
+const mapStateToProps = state => {
+  //Allows to access redux database
+  return {
+    user: state.user,
+    password: state.password,
+    auth_token: state.auth_token,
+    formdata: state.formdata,
+    mdmsdata: state.mdmsdata,
+    lang: state.lang
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  //Functions provided by redux
+  return {
+    edit_user: user => {
+      dispatch({ type: "EDIT_USERNAME", user: user });
+    },
+    edit_pass: password => {
+      dispatch({ type: "EDIT_PASSWORD", password: password });
+    },
+    edit_auth: auth_token => {
+      dispatch({ type: "AUTHENTICATE", auth_token: auth_token });
+    },
+    copy_form: formdata => {
+      dispatch({ type: "COPYFORMDETAILS", formdata: formdata });
+    },
+    load_data: mdmsdata => {
+      dispatch({ type: "DATALOAD", mdmsdata: mdmsdata });
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+
